68a5e516acab044bfdd6181d5a462bd8dafafeba
Amit <asagtani06@gmail.com>
2019-12-01 17:49:18 +0530

    initial version

--- tapsearch/__init__.py
+++ tapsearch/__init__.py
@@ -1,3 +1,9 @@
 from flask import Flask
+from tapsearch.indexer import Database, InvertedIndex
 import os
 app= Flask(__name__)
+app.config['SECRET_KEY'] = 'youwontguess'
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.static_folder = 'static'
+from tapsearch.routes import admin
+app.register_blueprint(admin)
